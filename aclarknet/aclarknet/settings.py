6901f3ee4704d8dbf5632ace47f7b1919596b904
Alex Clark <aclark@aclark.net>
2014-12-16 19:59:33 -0500

    Add static root

--- aclarknet/aclarknet/settings.py
+++ aclarknet/aclarknet/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -81,3 +80,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASEDIR, 'aclarknet', 'aclarknet', 'static')
