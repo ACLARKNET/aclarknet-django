855434523df57183c31ed9b10e7458232b79046a
Alex Clark <aclark@aclark.net>
2014-12-18 16:20:49 -0500

    Fix object name in Django Admin
    http://stackoverflow.com/questions/9336463/django-xxxxxx-object-display-customization-in-admin-action-sidebar

--- aclarknet/aclarknet/aclarknet/models.py
+++ aclarknet/aclarknet/aclarknet/models.py
@@ -4,10 +4,19 @@ from django.db import models
 class Client(models.Model):
     client_name = models.CharField(max_length=60)
 
+    def __unicode__(self):
+        return self.client_name
+
 
 class Service(models.Model):
     name = models.CharField(max_length=60)
 
+    def __unicode__(self):
+        return self.name
+
 
 class TeamMember(models.Model):
     name = models.CharField(max_length=60)
+
+    def __unicode__(self):
+        return self.name
