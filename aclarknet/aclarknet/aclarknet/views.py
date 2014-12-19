e893bef02d7e0700c1312363a89228c5aaa62de8
Alex Clark <aclark@aclark.net>
2014-12-18 19:43:28 -0500

    Add nav

--- aclarknet/aclarknet/aclarknet/views.py
+++ aclarknet/aclarknet/aclarknet/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def projects(request):
     return render(request, 'projects.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
--- aclarknet/aclarknet/urls.py
+++ aclarknet/aclarknet/urls.py
@@ -6,6 +6,9 @@ urlpatterns = patterns('',
 
     url(r'^$', 'aclarknet.aclarknet.views.home', name='home'),
     url(r'^projects/', 'aclarknet.aclarknet.views.projects', name='projects'),
+    url(r'^services/', 'aclarknet.aclarknet.views.services', name='services'),
+    url(r'^about/', 'aclarknet.aclarknet.views.about', name='about'),
+    url(r'^contact/', 'aclarknet.aclarknet.views.contact', name='contact'),
 
     # url(r'^blog/', include('blog.urls')),
 
