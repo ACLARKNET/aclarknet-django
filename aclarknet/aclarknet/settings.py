7f39418361410e5f858434d381f4260a2ba787e8
Alex Clark <aclark@aclark.net>
2015-01-03 08:23:11 -0500

    Update(s)

--- aclarknet/aclarknet/settings.py
+++ aclarknet/aclarknet/settings.py
@@ -93,7 +93,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'aclarknet', 'aclarknet', 'static')
 
 # Sendgrid
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get(SENDGRID_USERNAME, None)
-EMAIL_HOST_PASSWORD = os.environ.get(SENDGRID_PASSWORD, None)
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME', None)
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD', None)
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
