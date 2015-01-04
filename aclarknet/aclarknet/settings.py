65e9629c2d30f32d2b9cc119da27c40bb37257ba
Alex Clark <aclark@aclark.net>
2015-01-04 10:03:54 -0500

    Update

--- aclarknet/aclarknet/settings.py
+++ aclarknet/aclarknet/settings.py
@@ -98,4 +98,6 @@ EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'aclark@aclark.net'
-MANAGERS = 'aclark@aclark.net'
+MANAGERS = (
+    'aclark@aclark.net',
+)
