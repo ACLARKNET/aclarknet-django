c05a434ad16bd3ebde490e4a71118ddf667784df
Alex Clark <aclark@aclark.net>
2015-01-04 10:01:33 -0500

    Update

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
 dump:
 	curl -o latest.dump `heroku pgbackups:url`
-push:
-	git push
-	git push heroku master
 sync:
 	heroku run python aclarknet/manage.py syncdb
 up:
 	git commit -a -m "Update"
+	git push
+	git push heroku master
--- aclarknet/aclarknet/settings.py
+++ aclarknet/aclarknet/settings.py
@@ -97,3 +97,5 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'aclark@aclark.net'
+MANAGERS = 'aclark@aclark.net'
