81577a1911b4b215f0ff48b9e6feefa8756bce26
Alex Clark <aclark@aclark.net>
2015-01-06 09:55:17 -0500

    Clean up

--- aclarknet/aclarknet/aclarknet/views.py
+++ aclarknet/aclarknet/aclarknet/views.py
@@ -7,6 +7,10 @@ from aclarknet.aclarknet.models import TeamMember
 from aclarknet.aclarknet.models import Testimonial
 
 
+def about(request):
+    return render(request, 'about.html')
+
+
 def book(request):
     return render(request, 'book.html')
 
@@ -15,6 +19,30 @@ def clients(request):
     clients = Client.objects.all()
     context = {'clients': clients}
     return render(request, 'clients.html', context)
+
+
+def contact(request):
+    # https://docs.djangoproject.com/en/1.7/topics/forms/#the-view
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            from django.core.mail import send_mail
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['email']
+            recipients = ['aclark@aclark.net']
+            subject = 'Test'
+            send_mail(subject, message, sender, recipients)
+            # redirect to a new URL:
+            return HttpResponseRedirect('/contact/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact.html', context)
 
 
 def history(request):
@@ -50,31 +78,3 @@ def testimonials(request):
     testimonials = Testimonial.objects.order_by('-date')
     context = {'testimonials': testimonials}
     return render(request, 'testimonials.html', context)
-
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def contact(request):
-    # https://docs.djangoproject.com/en/1.7/topics/forms/#the-view
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = ContactForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            from django.core.mail import send_mail
-            message = form.cleaned_data['message']
-            sender = form.cleaned_data['email']
-            recipients = ['aclark@aclark.net']
-            subject = 'Test'
-            send_mail(subject, message, sender, recipients)
-            # redirect to a new URL:
-            return HttpResponseRedirect('/contact/')
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = ContactForm()
-    context = {'form': form}
-    return render(request, 'contact.html', context)
