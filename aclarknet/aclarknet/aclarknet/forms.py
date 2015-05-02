f6510984bfb0cef397df48d3fa431b0e7c66d8f8
Alex Clark <aclark@aclark.net>
2015-05-02 08:28:59 -0400

    Update

--- aclarknet/aclarknet/aclarknet/forms.py
+++ aclarknet/aclarknet/aclarknet/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    email = forms.CharField(label='Your email address (required):', widget=forms.TextInput(attrs={'class' : 'email'}))
+    email = forms.CharField(label='Your email address (required):', widget=forms.EmailInput(attrs={'class' : 'email'}))
     message = forms.CharField(label='How can we help (required):', widget=forms.Textarea(attrs={'class' : 'message'}))
     message2 = forms.CharField(label='How did you hear about us (optional):', required=False, widget=forms.Textarea(attrs={'class' : 'message2'}))
